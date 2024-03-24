1c2586226835c8e700325aaeb4aef211d1cf417d
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-03-25 04:48:41 +0900

    done

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const API_KEY = "41760a175c9d392190444c5d6c24b693";
-const BASE_PATH = "https://api.themoviedb.org/3/";
-
 interface IMovie {
   id: number;
   backdrop_path: string;
